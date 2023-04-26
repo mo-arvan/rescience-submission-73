769c2710b5f75bcbaa8c1c4b2e2f83b4e40d5e04
AugustinChrtn <augustin.chartouny@gmail.com>
2023-04-26 14:23:13 +0200

    small update on main

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ agent_parameters = {
     'BEB': {'gamma': 0.95, 'beta': 7, 'coeff_prior': 2, 'condition': 'informative'},
     'ζ-EB': {'gamma': 0.95, 'beta': 3, 'step_update': 10, 'alpha': 0.3, 'prior_LP': 0.01},
     'ζ-R-max': {'gamma': 0.95, 'Rmax': 1, 'm': 2, 'step_update': 10, 'alpha': 0.3, 'prior_LP': 0.01}}
-"""
+
 # Stationary environment
 environments = ['Lopes']
 nb_iters = 20
@@ -109,7 +109,7 @@ starting_seed = 400
 environments = ["Non_stat_strong_-0.1_{0}".format(non_stat)for non_stat in range(1, 21)]
 
 evaluate_agents(environments, agents, nb_iters, play_parameters, agent_parameters, starting_seed)
-"""
+
 # Without informative prior
 starting_seed = 250
 environments = ["Lopes"]
@@ -120,7 +120,7 @@ agent_parameters['BEB'] = {'gamma': 0.95, 'beta': 3, 'coeff_prior': 0.001, 'cond
 
 evaluate_agents(environments, agents, nb_iters, play_parameters, agent_parameters, starting_seed)
 
-"""
+
 # Replication with negative reward of -1
 
 agent_parameters = {
@@ -161,4 +161,3 @@ starting_seed = 2000
 environments = ["Non_stat_strong_-1_{0}".format(world)+'_{0}'.format(non_stat)
                 for world in range(1, 11) for non_stat in range(1, 11)]
 evaluate_agents(environments, agents, nb_iters, play_parameters, agent_parameters, starting_seed)
-"""
